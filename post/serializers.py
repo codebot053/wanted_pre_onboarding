128cb427030f0a88e6025309a23a40be0ed954a9
codebot053 <codebot053@gmail.com>
2022-10-18 15:59:34 +0900

    :sparkles: Create feature PostApiView(POST)
    채용공고를 등록할 수 있는 기능입니다. /post/ URI 를 통해 post요청으로 보낼때 채용공고를 등록할 수 있습니다.

--- post/serializers.py
+++ post/serializers.py
@@ -1,3 +1,6 @@
+# django
+from django.contrib.auth import get_user_model
+
 # DRF
 from rest_framework import serializers
 
@@ -12,7 +15,28 @@ class TechnologySerializer(serializers.ModelSerializer):
 
 class PostSerializer(serializers.ModelSerializer):
 
-    
+    technologies_needed = TechnologySerializer(many=True,required=False, read_only=True)
+    get_technology = serializers.ListField(required=False)
+
+    # 포스팅 생성(post)
+    def create(self, validated_data):
+        
+        get_technology = validated_data.pop('get_technology',[])
+        
+        job_post = Post.objects.create(**validated_data)
+        job_post.technologies_needed.add(*get_technology)
+        job_post.save()
+        return job_post
+
+    def validate(self, data):
+        user_id = data.get('author').id
+        user = get_user_model().objects.get(id=user_id)
+        if user.is_enterprise == False:
+            raise serializers.ValidationError(
+                detail={"error": "기업계정 사용자만 작성할 수 있습니다."},
+            )
+        return data
+        
     class Meta:
         model = Post
-        fields = ['company', 'position', 'job_reward', 'content', 'technologies_needed']
+        fields = ['author','company', 'position', 'job_reward', 'content', 'technologies_needed','get_technology']
--- post/views.py
+++ post/views.py
@@ -16,3 +16,12 @@ class PostApiView(APIView):
 
     def get(self,request):
         return Response(PostSerializer(Post.objects.all(), many=True).data, status=status.HTTP_200_OK)
+    def post(self, request):
+       
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
